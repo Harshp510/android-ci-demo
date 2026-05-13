f1c1cd24b379c0850d28d78dd67d6c5286790881
Harshp510 <harshp510@gmail.com>
2026-05-13 11:22:36 +0530

    Added version automation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,21 @@
+import java.util.Properties
+import java.io.FileInputStream
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     id("com.google.gms.google-services")
 }
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("keystore.properties")
+
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(keystorePropertiesFile.inputStream())
+}
+
+val versionPropsFile = rootProject.file("version.properties")
+val versionProps = Properties()
+
+versionProps.load(FileInputStream(versionPropsFile))
 
 android {
     namespace = "com.qntm.cicddemotest"
@@ -11,13 +24,21 @@ android {
             minorApiLevel = 1
         }
     }
+    signingConfigs {
+        create("release") {
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+        }
+    }
 
     defaultConfig {
         applicationId = "com.qntm.cicddemotest"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = versionProps["VERSION_CODE"].toString().toInt()
+        versionName = versionProps["VERSION_NAME"].toString()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -29,6 +50,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("release")
         }
     }
     compileOptions {
@@ -56,4 +78,27 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+}
+tasks.register("incrementVersionCode") {
+
+    doLast {
+
+        val versionPropsFile = rootProject.file("version.properties")
+
+        val versionProps = Properties()
+
+        versionProps.load(FileInputStream(versionPropsFile))
+
+        var versionCode = versionProps["VERSION_CODE"].toString().toInt()
+
+        versionCode++
+
+        versionProps["VERSION_CODE"] = versionCode.toString()
+
+        versionProps.store(versionPropsFile.writer(), null)
+
+        println("Updated VERSION_CODE to $versionCode")
+
+    }
+
 }
